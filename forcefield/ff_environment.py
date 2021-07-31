46f8f018eead9ce6584211f105a8621c62383f35
Morgane Moss <morganemoss@icloud.com>
2021-07-31 14:30:18 +0200

    make state 4-tuple

--- forcefield/ff_environment.py
+++ forcefield/ff_environment.py
@@ -2,17 +2,19 @@ import numpy as np
 
 class ForceField():
     
-    def __init__(self):
+    def __init__(self, start_pos = (.5, 1), goal = (.5, 0)):
         """Initialize forcefield environment.
         """
         
-        self.action_size = 2 # velocity in 2D space 
+        self.action_size = 2 # (x_velocity, y_velocity)
+        self.start_pos = start_pos 
+        self.goal = goal
         
-    def reset(self, pos=(1, .5)):
+    def reset(self, pos=(.5, 1)):
         """Reset the environment to the starting position. The start position is (1, .5) on a 2D coordinate system). 
         """
         
-        self.state = pos
+        self.state = pos + (0, 0) # State is 4 tuple (x_position, y_position, x_velocity, y_velocity).  
         return self 
         
     def act(self, action):
